632159a213038cbeb63212a79aa35d4e170a395e
Ben Juntilla <ben@benjuntilla.com>
2025-03-23 21:10:38 -0700

    disable env caching

--- apps/api/src/config.py
+++ apps/api/src/config.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-load_dotenv()
+load_dotenv(override=True)
 
 # If the environment is Gitpod, the root path will be the workspace cluster host
 # If not using gitpod, you can delete this if statement, but keep the else clause
